002657abf6160672425692f6bc2dfa3d87d4ff0d
ipercobaan <ipercobaan776@gmail.com>
2026-05-21 00:06:00 +0700

    Add files via upload

---
+++ src/data/defaultComments.ts
@@ -0,0 +1,44 @@
+import { GuestComment } from '../types';
+
+export const DEFAULT_COMMENTS: GuestComment[] = [
+  {
+    id: '1',
+    name: 'Teh Irma & Kang Yusuf',
+    relationship: 'Keluarga',
+    message: 'Barakallahu lakuma wa baraka alaikuma wa jamaa bainakuma fii khoir. Mugi lancar sagala rupina dugi ka dinten H ka Nisa geulis sareng Faisal kasep. Sakulawargi ngiring bingah pisan!',
+    isAttending: 'Hadir',
+    timestamp: '2026-05-18T08:30:00.000Z'
+  },
+  {
+    id: '2',
+    name: 'Aditia & Sarah (Kolektif Musik)',
+    relationship: 'Teman',
+    message: 'Happy wedding Ical & Nisa! Gila, pop-kalcer wedding of the year beneran ini mah. Piringan hitam aman? Haha. Sing sehat, lancar jaya mang Faisal dkk!',
+    isAttending: 'Hadir',
+    timestamp: '2026-05-19T14:15:00.000Z'
+  },
+  {
+    id: '3',
+    name: 'Alief (Teman Cipageran)',
+    relationship: 'Teman',
+    message: 'Wilujeng can! Akhirnya berlabuh juga di Cipageran Cimahi Utara. InsyaAllah hadir bareng barudak. Samawa bray!',
+    isAttending: 'Hadir',
+    timestamp: '2026-05-19T21:40:00.000Z'
+  },
+  {
+    id: '4',
+    name: 'Bu Rini & Pak Dadang',
+    relationship: 'Rekan',
+    message: 'Selamat mendampingi putra-putrinya Pak Agus Suhara dan Pak Supriatna. Semoga kedua mempelai senantiasa dalam limpahan berkah dan kelancaran menuju gerbang rumah tangga.',
+    isAttending: 'Hadir',
+    timestamp: '2026-05-20T03:10:00.000Z'
+  },
+  {
+    id: '5',
+    name: 'Gilang Gumelar',
+    relationship: 'Teman',
+    message: 'Congrats Faisal & Nisa! Mohon maaf belum bisa hadir karena masih ada tugas di luar kota, tapi doa terbaik dari kami sekeluarga selalu menyertai kalian berdua.',
+    isAttending: 'Tidak Hadir',
+    timestamp: '2026-05-20T10:05:00.000Z'
+  }
+];
